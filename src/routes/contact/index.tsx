424128db95d056e86e65466309fead9b932029ef
Justin Cox <jnx1017@gmail.com>
2026-03-06 10:41:37 -0600

    Switched to Spring hours

--- src/routes/contact/index.tsx
+++ src/routes/contact/index.tsx
@@ -73,7 +73,7 @@ export default component$(() => {
 						/>
 						<InfoCard
 							label="Current Hours"
-							value={store.value.currentHours.winter}
+							value={store.value.currentHours.spring}
 							icon={
 								<svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
 									<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />
